bcb3b68a9702bee5d72474740c60801c68040362
Vladislav Razoronov <razoronov@ucu.edu.ua>
2020-12-10 17:09:09 +0200

    update

--- src/main/java/ua/edu/ucu/stream/AsIntStream.java
+++ src/main/java/ua/edu/ucu/stream/AsIntStream.java
@@ -1,82 +1,80 @@
 package ua.edu.ucu.stream;
 
-import ua.edu.ucu.function.*;
+import ua.edu.ucu.
+        function. * ;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AsIntStream implements IntStream {
-    private List<Integer> numbers;
+    private List < Integer > numbers;
 
     private AsIntStream(int[] values) {
-        numbers = new ArrayList<>();
-        for(int number: values)numbers.add(number);
+        numbers = new ArrayList < >();
+        for (int number: values) numbers.add(number);
     }
 
-    private AsIntStream(List<Integer> values) {
-        numbers = new ArrayList<>();
+    private AsIntStream(List < Integer > values) {
+        numbers = new ArrayList < >();
         numbers.addAll(values);
     }
 
-    public static IntStream of(int... values) {
+    public static IntStream of(int...values) {
         return new AsIntStream(values);
     }
 
     @Override
     public Double average() {
-        return (double)sum()/(double)count();
+        return (double) sum() / (double) count();
     }
 
     @Override
     public Integer max() {
         int maximum = 0;
-        for(int number:numbers)
-            if(maximum<number)
-                maximum = number;
-            return  maximum;
+        for (int number: numbers)
+            if (maximum < number) maximum = number;
+        return maximum;
     }
 
     @Override
     public Integer min() {
         int minimum = Integer.MAX_VALUE;
-        for(int number:numbers)
-            if(minimum>number)
-                minimum = number;
-        return  minimum;
+        for (int number: numbers)
+            if (minimum > number) minimum = number;
+        return minimum;
     }
 
     @Override
     public long count() {
         return numbers.size();
-        }
+    }
 
     @Override
     public Integer sum() {
         Integer sum = 0;
-        for(Integer number:numbers) sum += number;
+        for (Integer number: numbers) sum += number;
         return sum;
     }
 
     @Override
     public IntStream filter(IntPredicate predicate) {
-        List<Integer> newNumbers = new ArrayList<>();
-        for(Integer number:numbers)
-            if(predicate.test(number))
-                newNumbers.add(number);
-            return new AsIntStream(newNumbers);
+        List < Integer > newNumbers = new ArrayList < >();
+        for (Integer number: numbers)
+            if (predicate.test(number)) newNumbers.add(number);
+        return new AsIntStream(newNumbers);
     }
 
     @Override
     public void forEach(IntConsumer action) {
-        for(int number: numbers){
+        for (int number: numbers) {
             action.accept(number);
         }
     }
 
     @Override
     public IntStream map(IntUnaryOperator mapper) {
-        List<Integer> newNumbers = new ArrayList<>();
-        for(int number: numbers){
+        List < Integer > newNumbers = new ArrayList < >();
+        for (int number: numbers) {
             newNumbers.add(mapper.apply(number));
         }
         return new AsIntStream(newNumbers);
@@ -84,9 +82,9 @@ public class AsIntStream implements IntStream {
 
     @Override
     public IntStream flatMap(IntToIntStreamFunction func) {
-        List<Integer> newNumbers = new ArrayList<>();
-        for(int number: numbers){
-            for(int streamNumber:func.applyAsIntStream(number).toArray()){
+        List < Integer > newNumbers = new ArrayList < >();
+        for (int number: numbers) {
+            for (int streamNumber: func.applyAsIntStream(number).toArray()) {
                 newNumbers.add(streamNumber);
             }
         }
@@ -95,7 +93,7 @@ public class AsIntStream implements IntStream {
 
     @Override
     public int reduce(int identity, IntBinaryOperator op) {
-        for(int number: numbers){
+        for (int number: numbers) {
             identity = op.apply(identity, number);
         }
         return identity;
@@ -103,8 +101,8 @@ public class AsIntStream implements IntStream {
 
     @Override
     public int[] toArray() {
-        int[] result = new int[(int)count()];
-        for(int i =0; i< count();i++){
+        int[] result = new int[(int) count()];
+        for (int i = 0; i < count(); i++) {
             result[i] = numbers.get(i);
         }
         return result;
--- src/test/java/ua/edu/ucu/StreamAppTest.java
+++ src/test/java/ua/edu/ucu/StreamAppTest.java
@@ -1,8 +1,8 @@
 package ua.edu.ucu;
 
-import ua.edu.ucu.stream.*;
+import ua.edu.ucu.stream. * ;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert. * ;
 import org.junit.Before;
 
 /**
@@ -10,29 +10,39 @@ import org.junit.Before;
  * @author andrii
  */
 public class StreamAppTest {
-    
+
     private IntStream intStream;
 
     @Before
     public void init() {
-        int[] intArr = {-1, 0, 1, 2, 3};
+        int[] intArr = { - 1,
+                0,
+                1,
+                2,
+                3
+        };
         intStream = AsIntStream.of(intArr);
     }
-    
+
     @Test
     public void testStreamOperations() {
         System.out.println("streamOperations");
         int expResult = 42;
         int result = StreamApp.streamOperations(intStream);
-        assertEquals(expResult, result);        
+        assertEquals(expResult, result);
     }
 
     @Test
     public void testStreamToArray() {
         System.out.println("streamToArray");
-        int[] expResult = {-1, 0, 1, 2, 3};
+        int[] expResult = { - 1,
+                0,
+                1,
+                2,
+                3
+        };
         int[] result = StreamApp.streamToArray(intStream);
-        assertArrayEquals(expResult, result);        
+        assertArrayEquals(expResult, result);
     }
 
     @Test
@@ -40,7 +50,34 @@ public class StreamAppTest {
         System.out.println("streamForEach");
         String expResult = "-10123";
         String result = StreamApp.streamForEach(intStream);
-        assertEquals(expResult, result);        
+        assertEquals(expResult, result);
     }
-    
+
+    @Test
+    public void testStreamSum() {
+        System.out.println("streamSum");
+        int expResult = 5;
+        int actResult = StreamApp.streamSum(intStream);
+        assertEquals(expResult, actResult);
+    }
+
+    @Test
+    public void testStreamMinMax() {
+        System.out.println("streamSum");
+        int expResultMin = -1;
+        int expResultMax = 3;
+        int actResultMin = StreamApp.streamMin(intStream);
+        int actResultMax = StreamApp.streamMax(intStream);
+        assertEquals(expResultMin, actResultMin);
+        assertEquals(expResultMax, actResultMax);
+    }
+
+    @Test
+    public void testStreamAverage() {
+        System.out.println("streamSum");
+        double expResult = 1.0;
+        double actResult = StreamApp.streamAvg(intStream);
+        assertEquals(expResult, actResult, 0.0001);
+    }
+
 }
